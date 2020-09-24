85aec6c017bc192539ea6be5f6b454c4327022e3
Lavish Aggarwal <lavishaggarwal91@yahoo.in>
2020-09-24 16:54:48 +0530

    removed unnecessary div and css

--- components/content.js
+++ components/content.js
@@ -7,28 +7,20 @@ export default function Content({ data }) {
                 data.length > 0 ? data.map((launch, index) => {
                     return (<div key={index} className={`${styles.missionContainerWidth} col-sm-12 col-md-6 col-lg-3`}>
                         <div className={styles.missionContainer}>
-                            <div style={{ backgroundColor: "rgb(241,242,242)", textAlign:"center" }}>
+                            <div style={{ backgroundColor: "rgb(241,242,242)", textAlign: "center" }}>
                                 <img className="img-fluid" src={launch.links.mission_patch_small} />
                             </div>
                             <br />
                             <div className={styles.missionName}>{launch.mission_name} #{launch.flight_number}</div>
                             <br />
-                            <div>
-                                <h5>Mission Ids:</h5> <ul>{launch.mission_id.map((id, index) => { return <li key={index}>{id}</li> })}</ul>
-                            </div>
-                            <div>
-                                <h5>Launch Year: <span className={styles.textColor}>{launch.launch_year}</span></h5>
-                            </div>
-                            <div>
-                                <h5>Successful Launch: <span className={styles.textColor}>{launch.launch_success ? "true" : "false"}</span></h5>
-                            </div>
-                            <div>
-                                <h5>Successful Landing: <span className={styles.textColor}>{launch.rocket.first_stage.cores[0].land_success ? "true" : "false"}</span></h5>
-                            </div>
+                            <h5>Mission Ids:</h5> <ul>{launch.mission_id.map((id, index) => { return <li key={index}>{id}</li> })}</ul>
+                            <h5>Launch Year: <span className={styles.textColor}>{launch.launch_year}</span></h5>
+                            <h5>Successful Launch: <span className={styles.textColor}>{launch.launch_success ? "true" : "false"}</span></h5>
+                            <h5>Successful Landing: <span className={styles.textColor}>{launch.rocket.first_stage.cores[0].land_success ? "true" : "false"}</span></h5>
                         </div>
                     </div>)
-                }) 
-                : 
+                })
+                :
                 <div className={`alert alert-warning ${styles.noRecordContainer}`}>No Records Found</div>
             }
         </div>
